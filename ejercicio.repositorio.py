575713144d1a76215ec67cfdcc4ce112885d907c
evejere <jeresanji@gmail.com>
2019-05-13 18:45:57 +0200

    agregue los comandos

--- ejercicio.repositorio.py
+++ ejercicio.repositorio.py
@@ -1,3 +1,18 @@
 #practica con control de versiones Git
 print("hola que tal ")
 
+
+"""
+comandos de GIT
+"""
+# git add
+# git commit -m "el mensaje"
+# git push      (para empujar el codigo del local al remoto)
+# git pull      (para estirar el codigo del remoto al local)
+"""
+comandos para navegar en la terminal
+"""
+#ls o dir (para ver documentos existentes)
+#cd nombre_de_archivo (para encontrar el archivo)
+#cd .. (para salir del archivo)
+
